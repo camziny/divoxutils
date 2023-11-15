972e8598f5a5f507d615ed311b269d7c2a61cbaa
camziny <cameronziny@gmail.com>
2023-11-14 20:08:10 -0500

    - adjust text on initial render - adjust character table styling

--- src/app/components/CharacterTableHeader.tsx
+++ src/app/components/CharacterTableHeader.tsx
@@ -3,36 +3,29 @@ import { TableCell, TableRow } from "@mui/material";
 function CharacterTableHeader() {
   return (
     <TableRow>
-      <TableCell className="header-text-color w-12 px-4" />{" "}
-      {/* Padding aligned with the container */}
-      <TableCell className="header-text-color w-32 px-4">Name</TableCell>{" "}
-      {/* Consistent padding */}
-      <TableCell className="header-text-color w-32 sm:w-auto sm:px-6">
+      <TableCell className="header-text-color w-12 px-4 sticky top-0 z-10 bg-gray-900" />
+      <TableCell className="header-text-color w-32 px-4 sticky top-0 z-10 bg-gray-900">
+        Name
+      </TableCell>
+      <TableCell className="header-text-color w-32 sm:w-auto sm:px-6 sticky top-0 z-10 bg-gray-900">
         Guild
-      </TableCell>{" "}
-      {/* Responsive width and padding */}
-      <TableCell className="header-text-color w-20 sm:w-auto sm:px-6">
+      </TableCell>
+      <TableCell className="header-text-color w-20 sm:w-auto sm:px-6 sticky top-0 z-10 bg-gray-900">
         Realm
-      </TableCell>{" "}
-      {/* Responsive width and padding */}
-      <TableCell className="header-text-color w-16 sm:w-auto sm:px-6">
+      </TableCell>
+      <TableCell className="header-text-color w-16 sm:w-auto sm:px-6 sticky top-0 z-10 bg-gray-900">
         Level
-      </TableCell>{" "}
-      {/* Responsive width and padding */}
-      <TableCell className="header-text-color w-24 lg:w-auto lg:px-8">
+      </TableCell>
+      <TableCell className="header-text-color w-24 lg:w-auto lg:px-8 sticky top-0 z-10 bg-gray-900">
         Race
-      </TableCell>{" "}
-      {/* Responsive width and padding */}
-      <TableCell className="header-text-color w-24 lg:w-auto lg:px-8">
+      </TableCell>
+      <TableCell className="header-text-color w-24 lg:w-auto lg:px-8 sticky top-0 z-10 bg-gray-900">
         Class
-      </TableCell>{" "}
-      {/* Responsive width and padding */}
-      <TableCell className="header-text-color w-24 px-4">
+      </TableCell>
+      <TableCell className="header-text-color w-24 px-4 sticky top-0 z-10 bg-gray-900">
         Realm Rank
-      </TableCell>{" "}
-      {/* Consistent padding */}
-      <TableCell className="header-text-color w-12 px-4" />{" "}
-      {/* Padding aligned with the container */}
+      </TableCell>
+      <TableCell className="header-text-color w-12 px-4 sticky top-0 z-10 bg-gray-900" />
     </TableRow>
   );
 }
