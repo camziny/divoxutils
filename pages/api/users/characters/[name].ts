14f247c33624ffb1410a596e4f45354357c3c935
camziny <cameronziny@gmail.com>
2024-02-28 12:06:07 -0500

    - add queries for realm and class type - add levenshtein util

--- pages/api/users/characters/[name].ts
+++ pages/api/users/characters/[name].ts
@@ -1,13 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getUserCharactersByUserName } from "@/controllers/userCharacterController";
+import {
+  getUserCharactersByUserName,
+  getAllUserNames,
+} from "@/controllers/userCharacterController";
 import {
   formatRealmRankWithLevel,
   getRealmRankForPoints,
 } from "@/utils/character";
+import {
+  characterClassesByClassType,
+  characterClassesByRealm,
+  Realm,
+  ClassType,
+} from "@/utils/group";
+import { findClosestMatch } from "@/utils/levenshtein";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const apiSecret = process.env.DISCORD_BOT_API_KEY;
-
   const apiKey = req.headers["x-api-key"];
 
   if (!apiKey || apiKey !== apiSecret) {
@@ -15,32 +24,68 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const { name } = req.query;
+  const capitalizeFirstLetter = (str: string) =>
+    str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+  const { name, realm, classType } = req.query;
+  const typedName = typeof name === "string" ? name : "";
+  const allUserNames = await getAllUserNames();
+  const closestUserName = findClosestMatch(typedName, allUserNames);
+  const typedClassType = classType as ClassType | undefined;
+  const realmAbbreviations: Record<string, string> = {
+    mid: "Midgard",
+    alb: "Albion",
+    hib: "Hibernia",
+  };
+  const typedRealm =
+    typeof realm === "string"
+      ? realmAbbreviations[realm.toLowerCase()] || realm
+      : undefined;
 
   if (!name || typeof name !== "string") {
     res.status(400).json({ message: "User name must be a string." });
     return;
   }
 
-  const userName = name;
-
   switch (req.method) {
     case "GET":
       try {
-        const userCharacters = await getUserCharactersByUserName(userName);
+        const userCharacters = await getUserCharactersByUserName(
+          closestUserName
+        );
         if (userCharacters && userCharacters.length > 0) {
-          const formattedCharacters = userCharacters.map((character) => {
-            const realmRank = getRealmRankForPoints(character.totalRealmPoints);
-            const formattedRank = formatRealmRankWithLevel(realmRank);
+          const filteredCharacters = userCharacters.filter((character) => {
+            const isRealmMatch = typedRealm
+              ? character.realm.toLowerCase() === typedRealm.toLowerCase()
+              : true;
+            const isTypeMatch = typedClassType
+              ? characterClassesByClassType[
+                  capitalizeFirstLetter(typedClassType) as ClassType
+                ].includes(character.className)
+              : true;
 
-            return {
-              characterName: character.characterName,
-              className: character.className,
-              formattedRank,
-            };
+            return isRealmMatch && isTypeMatch;
           });
 
-          res.status(200).json(formattedCharacters);
+          const sortedAndFormattedCharacters = filteredCharacters
+            .sort((a, b) => b.totalRealmPoints - a.totalRealmPoints)
+            .map((character) => {
+              const realmRank = getRealmRankForPoints(
+                character.totalRealmPoints
+              );
+              const formattedRank = formatRealmRankWithLevel(realmRank);
+
+              return {
+                characterName: character.characterName,
+                className: character.className,
+                formattedRank,
+              };
+            });
+
+          res.status(200).json({
+            user: `${closestUserName}`,
+            characters: sortedAndFormattedCharacters,
+          });
         } else {
           res
             .status(404)
