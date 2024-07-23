b5ba923e1f2ed1488d0f507c71ebf9876750b3d3
camziny <cameronziny@gmail.com>
2024-07-23 11:23:34 -0400

    import sortCharacters to both CharacterList and OtherCharacterList

--- src/app/components/OtherCharacterList.tsx
+++ src/app/components/OtherCharacterList.tsx
@@ -4,6 +4,7 @@ import AggregateStatistics from "./CharacterListSummary";
 import MobileCharacterTile from "./MobileCharacterTile";
 import { TableContainer, Paper, TableBody, Table } from "@mui/material";
 import { CharacterData, Realm } from "@/utils/character";
+import { sortCharacters } from "@/utils/sortCharacters";
 
 type OtherCharacterListProps = {
   userId?: string;
@@ -34,44 +35,6 @@ const realmOrder: Record<Realm, number> = {
   Albion: 1,
   Hibernia: 2,
   Midgard: 3,
-};
-
-const sortCharacters = (characters: CharacterData[], sortOption: string) => {
-  switch (sortOption) {
-    case "realm":
-      return characters.sort((a, b) => {
-        if (!(a.realm in realmOrder) || !(b.realm in realmOrder)) {
-          console.error("Invalid realm data:", a.realm, b.realm);
-          return 0;
-        }
-        if (realmOrder[a.realm] < realmOrder[b.realm]) return -1;
-        if (realmOrder[a.realm] > realmOrder[b.realm]) return 1;
-        if (
-          typeof a.heraldRealmPoints !== "number" ||
-          typeof b.heraldRealmPoints !== "number"
-        ) {
-          console.error(
-            "Invalid realm points data:",
-            a.heraldRealmPoints,
-            b.heraldRealmPoints
-          );
-          return 0;
-        }
-        return b.heraldRealmPoints - a.heraldRealmPoints;
-      });
-
-    case "rank-high-to-low":
-      return characters.sort(
-        (a, b) => b.heraldRealmPoints - a.heraldRealmPoints
-      );
-
-    case "rank-low-to-high":
-      return characters.sort(
-        (a, b) => a.heraldRealmPoints - b.heraldRealmPoints
-      );
-    default:
-      return characters;
-  }
 };
 
 export default async function OtherCharacterList({
