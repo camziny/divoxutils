43d553cb4ad475f99715f6f92566b7460f8fa2b1
camziny <cameronziny@gmail.com>
2025-07-09 19:20:37 -0400

    - change note

--- src/app/components/CharacterSearchAndAdd.tsx
+++ src/app/components/CharacterSearchAndAdd.tsx
@@ -236,7 +236,7 @@ function CharacterSearchAndAdd() {
 
       <div className="mb-4 px-1">
         <div className="bg-gray-800/50 border border-gray-600/30 text-gray-400 px-3 py-2 rounded-md text-xs flex items-center gap-2">
-          <span>Note: Performance improvements are currently underway. Your character list may take a moment to update after adding / deleting characters. Thanks for your patience!</span>
+          <span>Note: Your character list may take a moment to update after adding / deleting characters, but this is temporary. Thanks for your patience!</span>
         </div>
       </div>
 
