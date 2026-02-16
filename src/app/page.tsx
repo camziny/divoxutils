bc9425a962c9d2c675f9d965a9d3f4be8087a51c
camziny <cameronziny@gmail.com>
2026-02-16 13:47:55 -0500

    - navbar update - update hover effect on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,10 @@ export default function HomePage() {
         <header className="text-center py-16">
           <div className="relative">
             <h1 className="text-5xl sm:text-6xl lg:text-7xl font-black mb-4 tracking-tight">
-              <Link href="/user/divox/characters">
-                <div className="group inline-block relative">
-                  <span className="text-indigo-400 hover:text-indigo-300 transition-all duration-300 transform group-hover:scale-105">
-                    divox
-                  </span>
-                  <div className="absolute -inset-2 bg-indigo-600/10 rounded-lg blur-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-                </div>
+              <Link href="/user/divox/characters" className="text-indigo-400 hover:text-indigo-300 transition-colors">
+                divox
               </Link>
-              <span className="text-white drop-shadow-lg">utils</span>
+              <span className="text-white">utils</span>
             </h1>
             <div className="w-24 h-1 bg-indigo-500 mx-auto rounded-full"></div>
           </div>
